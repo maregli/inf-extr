80d391612554efe8b62a446eca2034fc35ff2f0b
Marc Egli <eglimar@ethz.ch>
2023-10-27 16:37:57 +0200

    prepared data questions nb

---
+++ src/data/data_loader.py
@@ -0,0 +1,3 @@
+from ..paths import DATA_PATH
+def data_loader():
+    return DATA_PATH
---
+++ src/paths.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import os
+
+# Project Root
+PROJECT_ROOT = Path("/cluster/dataset/midatams")
+
+# Data Path
+DATA_PATH = PROJECT_ROOT / "data"

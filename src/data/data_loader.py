018dc17a1567f4470c22bf6a5c423ce9f251297a
maregli <marc_e@hispeed.ch>
2023-10-31 17:17:42 +0100

    preparing for BERT embedding

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,3 +1,30 @@
-from ..paths import DATA_PATH
-def data_loader():
-    return DATA_PATH
+from ..paths import *
+import pandas as pd
+import os
+
+def get_nested_csv(dir_name: str, file_name: str):
+    """
+    Returns a joint pandas dataframe from the files matching file_name
+    in all the different import dates subdirectories of the directory
+    specified by dir_name
+    
+    :param dir_name: The name of the directory (e.g. "seantis")
+    :param file_name: The name of the csv file to be read. (e.g. "diagnoses.csv")
+    """
+    list_dfs = []
+
+    for root, dirs, files in os.walk(os.path.join(DATA_PATH_RAW, dir_name)):
+        
+        if root.split(os.sep)[-1].startswith("imported_"):
+            try:
+                _df = pd.read_csv(os.path.join(root, file_name))
+                list_dfs.append(_df)
+            except FileNotFoundError:
+                print(f"File not found in: {root}")
+                continue
+            except UnicodeDecodeError:
+                print(f"UnicodeDecodeError in: {root}")
+                continue
+
+    df = pd.concat(list_dfs)
+    return df

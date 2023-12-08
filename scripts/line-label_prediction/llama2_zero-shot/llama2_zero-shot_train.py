8f98b2f189360e637e160f1ced901d9d3393256b
Marc Egli <eglimar@ethz.ch>
2023-12-08 11:53:34 +0100

    looking at test slurm job

--- scripts/line-label_prediction/llama2_zero-shot/llama2_zero-shot_train.py
+++ scripts/line-label_prediction/llama2_zero-shot/llama2_zero-shot_train.py
@@ -1,6 +1,12 @@
 import numpy as np
+import time
 
 print("Numpy version: ", np.__version__)
 
 a = np.array([1, 2, 3])
 print("a: ", a)
+
+# Count to 50 with 1 second delay
+for i in range(50):
+    print(i)
+    time.sleep(1)

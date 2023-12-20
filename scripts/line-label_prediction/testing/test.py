95d5d6cdfc1a72aed4a56b8e5c6a5079459f4588
Marc Egli <eglimar@ethz.ch>
2023-12-20 14:44:27 +0100

    adapted test for batch job

--- scripts/line-label_prediction/testing/test.py
+++ scripts/line-label_prediction/testing/test.py
@@ -1,6 +1,17 @@
 import numpy as np
+
+import pandas as pd
+
 import time
+
 import torch
+
+import os
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent.parent.parent))
+
+from src import paths
 
 print("Numpy version: ", np.__version__)
 
@@ -13,7 +24,11 @@ print("GPU device name: ", torch.cuda.get_device_name(0))
 print("Number of GPUs: ", torch.cuda.device_count())
 print("GPU memory per device: ", torch.cuda.get_device_properties("cuda:0"))
 
-# Count to 50 with 1 second delay
-for i in range(50):
+# Save a list of numbers to a file
+b = ["blabla", "blabla2", "blabla3"]
+pd.Series(b).to_csv(paths.RESULTS_PATH/"test.csv", index=False, header=False)
+
+# Count to 30 with 1 second delay
+for i in range(30):
     print(i)
     time.sleep(1)

cd9ba2fabbe3f4a04545553c3adba57158c2ab3a
Marc Egli <eglimar@ethz.ch>
2023-12-25 13:37:00 +0100

    got batch job working again

---
+++ scripts/ms-diag/llama2-chat/job-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+#SBATCH --job-name=test-permission
+#SBATCH --output=/cluster/home/eglimar/inf-extr/scripts/ms-diag/llama2-chat/logs/test-permission-%j.out
+#SBATCH --error=/cluster/home/eglimar/inf-extr/scripts/ms-diag/llama2-chat/logs/test-permission-%j.err
+#SBATCH --cpus-per-task=1
+#SBATCH -p gpu
+#SBATCH --gres=gpu:rtx3090:1
+#SBATCH --time=7:00:00
+#SBATCH --mem-per-cpu=10G
+ 
+source ~/.bashrc
+conda activate inf-extr
+
+echo "Starting job with ID $SLURM_JOB_ID..."
+python /cluster/home/eglimar/inf-extr/scripts/ms-diag/llama2-chat/test-permission.py
+echo "Job finished"

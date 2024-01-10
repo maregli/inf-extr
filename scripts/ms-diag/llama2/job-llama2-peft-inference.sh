cf5c0a29520d179f24c3c56c9528c9b81bd7e9d5
Marc Egli <eglimar@ethz.ch>
2024-01-10 14:26:34 +0100

    updated job script

--- scripts/ms-diag/llama2/job-llama2-peft-inference.sh
+++ scripts/ms-diag/llama2/job-llama2-peft-inference.sh
@@ -12,19 +12,6 @@
 source ~/.bashrc
 conda activate inf-extr
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Zero Shot Classification with Llama2-Chat")
-    parser.add_argument("--job_id", type=str, default="unknown", help="Job ID")
-    parser.add_argument("--model_name", type=str, default=MODEL_NAME, help="Name of model to be used. Defaults to llama2. Must be saved in the path: paths.MODEL_PATH/model_name")
-    parser.add_argument("--peft_model_names", nargs="+", type=str, help="List of PEFT models for which to perform inference. Must be saved in the path: paths.MODEL_PATH/model_name. Must be compatible with base model.")
-    parser.add_argument("--quantization", type=str, default=QUANTIZATION, help="Quantization. Must be one of 4bit, bfloat16 or float16. Defaults to 4bit")
-    parser.add_argument("--batch_size", type=int, default=BATCH_SIZE, help="Batch Size. Defaults to 4")
-    parser.add_argument("--split", type=str, default=SPLIT, help="Split. Must be one of train, validation or test. Defaults to train")
-    parser.add_argument("--num_labels", type=int, default=NUM_LABELS, help="Number of labels (classes) to predict. Defaults to 3. Must be compatible with base model.")
-
-    args = parser.parse_args()
-
-
 echo "Starting job with ID $SLURM_JOB_ID..."
 python /cluster/home/eglimar/inf-extr/scripts/ms-diag/llama2/peft-inference.py \
     --job_id $SLURM_JOB_ID \

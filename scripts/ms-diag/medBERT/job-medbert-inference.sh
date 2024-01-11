7e93021990c47c10214fed7197604e391675aecc
Marc Egli <eglimar@ethz.ch>
2024-01-11 11:59:23 +0100

    medbert ms inference job script

--- scripts/ms-diag/medBERT/job-medbert-inference.sh
+++ scripts/ms-diag/medBERT/job-medbert-inference.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
 #SBATCH --job-name=medbert_inference
-#SBATCH --output=/cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/logs/inference_original-%j.out
-#SBATCH --error=/cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/logs/inference_original-%j.err
+#SBATCH --output=/cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/logs/inference-%j.out
+#SBATCH --error=/cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/logs/inference-%j.err
 #SBATCH --cpus-per-task=1
 #SBATCH -p gpu
 #SBATCH --gres=gpu:rtx1080ti:1
-#SBATCH --time=00:10:00
+#SBATCH --time=00:15:00
 #SBATCH --mem-per-cpu=10G
  
 source ~/.bashrc
@@ -15,5 +15,11 @@ conda activate inf-extr
 echo "Starting job with ID $SLURM_JOB_ID..."
 python /cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/inference.py \
     --model_name ms-diag_medbert_bfloat16_finetuned_original_128 \
+    --quantization bfloat16 
+python /cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/inference.py \
+    --model_name ms-diag_medbert_bfloat16_finetuned_augmented_128 \
+    --quantization bfloat16 
+python /cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/inference.py \
+    --model_name ms-diag_medbert_bfloat16_finetuned_zero-shot_128 \
     --quantization bfloat16 
 echo "Job finished"

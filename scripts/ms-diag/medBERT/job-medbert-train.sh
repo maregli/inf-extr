5e9ed6ba87df324a6cb0c74b00a2cc194bb5c3c5
Marc Egli <eglimar@ethz.ch>
2024-01-11 11:44:21 +0100

    medbert training script

---
+++ scripts/ms-diag/medBERT/job-medbert-train.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+ 
+#SBATCH --job-name=medbert_train
+#SBATCH --output=/cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/logs/train-%j.out
+#SBATCH --error=/cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/logs/train-%j.err
+#SBATCH --cpus-per-task=1
+#SBATCH -p gpu
+#SBATCH --gres=gpu:rtx1080ti:1
+#SBATCH --time=00:20:00
+#SBATCH --mem-per-cpu=10G
+ 
+source ~/.bashrc
+conda activate inf-extr
+
+echo "Starting job with ID $SLURM_JOB_ID..."
+python /cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/train.py \
+    --quantization bfloat16 \
+    --lr 0.0001 \
+    --data original \
+    --truncation_size 128 \
+    --num_epochs 8
+python /cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/train.py \
+    --quantization bfloat16 \
+    --lr 0.0001 \
+    --data zero-shot \
+    --truncation_size 128 \
+    --num_epochs 8
+python /cluster/home/eglimar/inf-extr/scripts/ms-diag/medBERT/train.py \
+    --quantization bfloat16 \
+    --lr 0.0001 \
+    --data augmented \
+    --truncation_size 128 \
+    --num_epochs 8
+echo "Job finished"

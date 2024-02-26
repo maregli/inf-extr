ec126148ce07219365e40281f4351669b75a787f
Marc Egli <eglimar@ethz.ch>
2024-02-26 08:16:09 +0100

    pipeline job line classifier

--- scripts/line-label/medbert/job-medbert-pipeline.sh
+++ scripts/line-label/medbert/job-medbert-pipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
  
-#SBATCH --job-name=medbert_line-label_class
-#SBATCH --output=/cluster/home/eglimar/inf-extr/scripts/line-label/medbert/logs/class-%j.out
-#SBATCH --error=/cluster/home/eglimar/inf-extr/scripts/line-label/medbert/logs/class-%j.err
+#SBATCH --job-name=medbert_line-label_class_pipeline
+#SBATCH --output=/cluster/home/eglimar/inf-extr/scripts/line-label/medbert/logs/class-pipeline-%j.out
+#SBATCH --error=/cluster/home/eglimar/inf-extr/scripts/line-label/medbert/logs/class-pipeline-%j.err
 #SBATCH --cpus-per-task=1
 #SBATCH -p gpu
 #SBATCH --gres=gpu:rtx1080ti:1
@@ -18,5 +18,6 @@ python /cluster/home/eglimar/inf-extr/scripts/line-label/finetune.py \
     --task_type class \
     --batch_size 16 \
     --lr 2e-5 \
-    --num_epochs 20
+    --num_epochs 20 \
+    --data_version pipeline
 echo "Job finished"
